29eb45a1cc1ec26db04111b49632430e7d23e493
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-04-04 12:43:39 +0200

    Agrego la funcion typewriter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
+import Typewriter from "typewriter-effect";
 import "../styles/_Home.scss";
+
 const Home = () => {
 	return (
 		<section>
 			<h1 className="titulo-home">maria victoria diaz</h1>
+			<h2>i'm a</h2>
+			<Typewriter
+				options={{
+					strings: ["Frontend Developer"],
+					autoStart: true,
+					loop: true,
+				}}
+			/>
 		</section>
 	);
 };
