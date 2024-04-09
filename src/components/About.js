23956bf23a7559062bd292abc491af5d799af005
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-04-09 20:17:08 +0200

    Creo archivo about,images, agrego estilos de la seccion about

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,23 @@
+import fotoAbout from "../images/fotoAbout.jpg";
+import "../styles/_About.scss";
 const About = () => {
-	return <div></div>;
+	return (
+		<div>
+			<section className="container-about">
+				<p>
+					Me considero una persona metódica, que disfruta de trabajar en equipo
+					y alcanzar nuevos objetivos, durante la pandemia comencé a interesarme
+					en la programación, al principio viendo cursos en Youtube y luego
+					buscando una manera formal de ampliar mis conocimientos. En esta
+					búsqueda comencé con mis estudios en ADA ITW, aprendiendo y aplicando
+					tecnologías tales como HTML, CSS, JS, REACT, GIT, SASS, Material UI y
+					APIs. Actualmente habiendo egresado de ADA sigo trabajando día a día
+					en mejorar mis proyectos y ampliar mis conocimientos, en búsqueda de
+					una nueva oportunidad de aplicarlos en el ámbito laboral.
+				</p>
+				<img alt="foto de perfil" src={fotoAbout} />
+			</section>
+		</div>
+	);
 };
 export default About;
