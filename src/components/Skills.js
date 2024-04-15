a2c31322d95b11d13735a78b52722a269b963c69
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-04-15 20:38:13 +0200

    Corrijo atributo classname

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,46 +10,46 @@ const Skills = () => {
 			</p>
 			<h4>Languages</h4>
 			<div className="languages">
-				<i class="devicon-html5-plain colored">
+				<i className="devicon-html5-plain colored">
 					<p>html5</p>
 				</i>
-				<i class="devicon-css3-plain colored">
+				<i className="devicon-css3-plain colored">
 					<p>css3</p>
 				</i>
-				<i class="devicon-javascript-plain colored">
+				<i className="devicon-javascript-plain colored">
 					<p>javascript</p>
 				</i>
 			</div>
 			<h4>frameworks</h4>
 			<div className="frameworks">
-				<i class="devicon-react-original colored">
+				<i className="devicon-react-original colored">
 					<p>react</p>
 				</i>
-				<i class="devicon-sass-original colored">
+				<i className="devicon-sass-original colored">
 					<p>sass</p>
 				</i>
-				<i class="devicon-materialui-plain colored">
+				<i className="devicon-materialui-plain colored">
 					<p>material ui</p>
 				</i>
-				<i class="devicon-bulma-plain colored">
+				<i className="devicon-bulma-plain colored">
 					<p>bulma</p>
 				</i>
 			</div>
 			<h4>tools</h4>
 			<div className="tools">
-				<i class="devicon-git-plain colored">
+				<i className="devicon-git-plain colored">
 					<p>git</p>
 				</i>
-				<i class="devicon-github-original colored">
+				<i className="devicon-github-original colored">
 					<p>github</p>
 				</i>
-				<i class="devicon-trello-plain-wordmark colored">
+				<i className="devicon-trello-plain-wordmark colored">
 					<p>trello</p>
 				</i>
-				<i class="devicon-postman-plain colored">
+				<i className="devicon-postman-plain colored">
 					<p>postman</p>
 				</i>
-				<i class="devicon-apple-original">
+				<i className="devicon-apple-original">
 					<p>ios</p>
 				</i>
 			</div>
