8698f10d8123b863457ff7e41ebcc6aa618f8733
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-04-16 21:51:17 +0200

    Agrego componente de proyectos e imagenes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,29 @@
+import buscadorPeliculas from "../images/buscadorPeliculas.png";
+
 const Projects = () => {
-	return <div></div>;
+	return (
+		<div>
+			<h1>projects</h1>
+			<div>
+				<img src={buscadorPeliculas} alt="buscador de peliculas" />
+				<h3>buscador de peliculas</h3>
+				<a
+					href="https://diazvic-buscador-de-peliculas.netlify.app/"
+					target="_blanck"
+				>
+					<button>live</button>
+				</a>
+				<a
+					href="https://github.com/diazvic/buscador-de-peliculas"
+					target="_blanck"
+				>
+					<button>github</button>
+				</a>
+				<span>react</span>
+				<span>sass</span>
+				<span>material ui</span>
+			</div>
+		</div>
+	);
 };
 export default Projects;
