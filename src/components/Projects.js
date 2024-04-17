3697a6a5dcbf6425ff76a9a9bc73c70bee556e7e
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-04-17 13:17:18 +0200

    Agrego card de proyectos y estilos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,30 @@
+import "../styles/_Projects.scss";
 import buscadorPeliculas from "../images/buscadorPeliculas.png";
 
 const Projects = () => {
 	return (
 		<div>
 			<h1>projects</h1>
-			<div>
-				<img src={buscadorPeliculas} alt="buscador de peliculas" />
-				<h3>buscador de peliculas</h3>
-				<a
-					href="https://diazvic-buscador-de-peliculas.netlify.app/"
-					target="_blanck"
-				>
-					<button>live</button>
-				</a>
-				<a
-					href="https://github.com/diazvic/buscador-de-peliculas"
-					target="_blanck"
-				>
-					<button>github</button>
-				</a>
-				<span>react</span>
-				<span>sass</span>
-				<span>material ui</span>
+			<div className="container-projects">
+				<div className="box-projects">
+					<img src={buscadorPeliculas} alt="buscador de peliculas" />
+					<h3>buscador de peliculas</h3>
+					<a
+						href="https://diazvic-buscador-de-peliculas.netlify.app/"
+						target="_blanck"
+					>
+						<button>live</button>
+					</a>
+					<a
+						href="https://github.com/diazvic/buscador-de-peliculas"
+						target="_blanck"
+					>
+						<button>github</button>
+					</a>
+					<span className="react">react</span>
+					<span className="sass">sass</span>
+					<span className="mui">material ui</span>
+				</div>
 			</div>
 		</div>
 	);
