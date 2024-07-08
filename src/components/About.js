a983f41c252dd63802c9ad1e0ba31aaf58dea429
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-07-08 14:07:43 +0200

    Cambios en el componente About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,22 @@ import fotoAbout from "../images/fotoAbout.jpg";
 import "../styles/_About.scss";
 const About = () => {
 	return (
-		<div>
-			<section className="container-about">
-				<p>
-					Me considero una persona metódica, que disfruta de trabajar en equipo
-					y alcanzar nuevos objetivos, durante la pandemia comencé a interesarme
-					en la programación, al principio viendo cursos en Youtube y luego
-					buscando una manera formal de ampliar mis conocimientos. En esta
-					búsqueda comencé con mis estudios en ADA ITW, aprendiendo y aplicando
-					tecnologías tales como HTML, CSS, JS, REACT, GIT, SASS, Material UI y
-					APIs. Actualmente habiendo egresado de ADA sigo trabajando día a día
-					en mejorar mis proyectos y ampliar mis conocimientos, en búsqueda de
-					una nueva oportunidad de aplicarlos en el ámbito laboral.
-				</p>
+		<section className="container-about">
+			<p>
+				Me considero una persona metódica, que disfruta de trabajar en equipo y
+				alcanzar nuevos objetivos, durante la pandemia comencé a interesarme en
+				la programación, al principio viendo cursos en Youtube y luego buscando
+				una manera formal de ampliar mis conocimientos. En esta búsqueda comencé
+				con mis estudios en ADA ITW, aprendiendo y aplicando tecnologías tales
+				como HTML, CSS, JS, REACT, GIT, SASS, Material UI y APIs. Actualmente
+				habiendo egresado de ADA sigo trabajando día a día en mejorar mis
+				proyectos y ampliar mis conocimientos, en búsqueda de una nueva
+				oportunidad de aplicarlos en el ámbito laboral.
+			</p>
+			<div className="box-img">
 				<img alt="foto de perfil" src={fotoAbout} />
-			</section>
-		</div>
+			</div>
+		</section>
 	);
 };
 export default About;
